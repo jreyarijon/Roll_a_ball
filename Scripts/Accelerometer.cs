89a9dcedc45488c283d6dfe5b035f19993c12f2a
Josemolamazo <Jreyarijon@danielcastelao.org>
2021-01-18 11:37:25 +0100

    Creado script Enemy y agregado componente NavMeshAgent

---
+++ Scripts/Accelerometer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(Input.acceleration);
+    }
+}
